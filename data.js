a43c91e6dd2a48dc819ca2073f735199650f0eb0
xichika <47033378+xichika@users.noreply.github.com>
2019-02-03 13:25:36 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,96 @@
+new TypeIt('#boogiepop', {
+  speed: 45
+})
+.type('Eps <b>06</b>');
+
+new TypeIt('#domekano', {
+  speed: 45
+})
+.type('Eps <b>04</b>');
+
+new TypeIt('#dororo', {
+  speed: 45
+})
+.type('Eps <b>04</b>');
+
+new TypeIt('#egao', {
+  speed: 45
+})
+.type('Eps <b>05</b>');
+
+new TypeIt('#gaf', {
+  speed: 45
+})
+.type('Eps <b>04</b>');
+
+new TypeIt('#gnh', {
+  speed: 45
+})
+.type('Eps <b>04</b>');
+
+new TypeIt('#grimms', {
+  speed: 45
+})
+.type('Eps <b>04</b>');
+
+new TypeIt('#kkg', {
+  speed: 45
+})
+.type('Eps <b>04</b>');
+
+new TypeIt('#asuka', {
+  speed: 45
+})
+.type('Eps <b>04</b>');
+
+new TypeIt('#mob', {
+  speed: 45
+})
+.type('Eps <b>04</b>');
+
+new TypeIt('#sao', {
+  speed: 45
+})
+.type('Eps <b>17</b>');
+
+new TypeIt('#tate', {
+  speed: 45
+})
+.type('Eps <b>04</b>');
+
+new TypeIt('#slime', {
+  speed: 45
+})
+.type('Eps <b>17</b>');
+
+new TypeIt('#wz', {
+  speed: 45
+})
+.type('Eps <b>04</b>');
+
+new TypeIt('#ynn', {
+  speed: 45
+})
+.type('Eps <b>04</b>');
+
+new TypeIt('#uncom', {
+  speed: 45
+})
+.type('Masih dalam tahap pengumpulan');
+
+new TypeIt('#info', {
+  speed: 45
+})
+.type('Dalam koleksi ini gw cuma upload video anime beresolusi 720p dan 1080p saja.')
+.break()
+.type('Jika ente bahlul dan suka download resolusi burem seperti 360p/480p maka bukan disini tempatnya gaes dan disini gak update anime berepisode banyak seperti One Piece, Boruto, atau Fairi Tail karena udah ketinggalan jauh hhhh')
+.break()
+.break()
+.type('Semua link download anime disini memakai google drive agar wuzz dan untuk fileya akan otomatis update sendiri jika gw upload anime episode terbarunya.')
+.break()
+.break()
+.type('Jika ente bingung dengan episode yang baru rilis itu episode berapa, silahkan masuk ke situs dibawah ini untuk melihat info nomor episode terbarunya:')
+.break()
+.type('- https://horriblesubs.info')
+.break()
+.type('- https://torrents.ohys.net/t/');

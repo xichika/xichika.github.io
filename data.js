d892a86fc7100c3a3ca608d6b188d5a090b5a984
xichika <47033378+xichika@users.noreply.github.com>
2019-02-07 06:46:04 +0700

    Update

--- data.js
+++ data.js
@@ -73,11 +73,9 @@ new TypeIt('#ynn', {
 })
 .type('Eps <b>04</b>');
 
-new TypeIt('#uncom', {
+new TypeIt('#reso', {
   speed: 45
 })
-.type('Masih dalam tahap pengumpulan data')
-.break()
 .type('- [HADE 10bit] adalah 720p 10bit')
 .break()
 .type('- [S-HADE] adalah 1080p');
@@ -95,7 +93,7 @@ new TypeIt('#info', {
 .type('Jika ente bahlul dan suka download resolusi burem seperti 360p/480p maka bukan disini tempatnya gaes dan disini tidak update anime episode banyak seperti One Piece, Boruto, Black Clover atau Fairy Tail karena udah ketinggalan jauh hhhh')
 .break()
 .break()
-.type('Semua link download anime disini memakai google drive agar wuzz dan untuk filenya akan otomatis update sendiri jika gw upload videonya.')
+.type('Semua link download anime disini memakai google drive agar wuzz dan untuk list file episodenya akan otomatis update sendiri jika gw upload videonya.')
 .break()
 .break()
 .type('Jika ente bingung dengan episode yang baru rilis itu episode berapa, silahkan masuk ke situs dibawah ini untuk melihat info episode terbarunya:')

56951a8c508d4f2c5d5893fd6b572bb6a320a765
xichika <47033378+xichika@users.noreply.github.com>
2019-02-15 19:25:31 +0700

    Informasi JS

---
+++ js/infoku.js
@@ -0,0 +1,23 @@
+new TypeIt('#reso', {
+  speed: 25
+})
+.type('- [HADE 10bit] adalah 720p 10bit')
+.break()
+.type('- [S-HADE] adalah 1080p');
+
+new TypeIt('#info', {
+  speed: 45
+})
+.type('Dalam koleksi ini gw cuma upload video anime beresolusi 720p dan 1080p saja.')
+.break()
+.type('Jika ente bahlul dan suka download resolusi burem seperti 360p/480p maka bukan disini tempatnya gaes dan disini tidak update anime episode banyak seperti One Piece, Boruto, Black Clover atau Fairy Tail karena udah ketinggalan jauh hhhh')
+.break()
+.break()
+.type('Semua link download anime disini memakai google drive agar wuzz dan untuk list file episodenya akan otomatis update sendiri jika gw upload videonya.')
+.break()
+.break()
+.type('Jika ente bingung dengan episode yang baru rilis itu episode berapa, silahkan masuk ke situs dibawah ini untuk melihat info episode terbarunya:')
+.break()
+.type('- https://horriblesubs.info')
+.break()
+.type('- https://torrents.ohys.net/t/');

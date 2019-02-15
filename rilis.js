e2a222a6aa4d4c917edf67e5c6028bd59b209c78
xichika <47033378+xichika@users.noreply.github.com>
2019-02-15 19:40:30 +0700

    New Release

---
+++ rilis.js
@@ -0,0 +1,10 @@
+new TypeIt('#update', {
+  speed: 45
+})
+.type('- Gotoubun no Hanayome Eps 06')
+.break()
+.type('- Yakusoku no Neverland Eps 06')
+.break()
+.type('- Girly Air Force Eps 06')
+.break()
+.type('- Tate no Yuusha no Nariagari Eps 06');

c64c80615c6a09982da365e803bf4ba5fdbd9304
xichika <47033378+xichika@users.noreply.github.com>
2019-02-15 08:26:21 +0700

    Gotoubun no Hanayome 06

--- data.js
+++ data.js
@@ -31,7 +31,7 @@ new TypeIt('#gaf', {
 new TypeIt('#gnh', {
   speed: 25
 })
-.type('Eps <b>05</b>');
+.type('Eps <b>06</b>');
 
 new TypeIt('#grimms', {
   speed: 25
@@ -88,6 +88,8 @@ new TypeIt('#reso', {
 new TypeIt('#update', {
   speed: 45
 })
+.type('- Gotoubun no Hanayome Eps 06')
+.break()
 .type('- Yakusoku no Neverland Eps 06')
 .break()
 .type('- Girly Air Force Eps 06')

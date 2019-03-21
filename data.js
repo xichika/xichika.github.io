27fcc1d0f53a3198bbf275b0bb23abfec9afc598
xichika <47033378+xichika@users.noreply.github.com>
2019-03-22 00:38:29 +0700

    Kakegurui×× 11

--- data.js
+++ data.js
@@ -41,7 +41,7 @@ new TypeIt('#grimms', {
 new TypeIt('#kkg', {
   speed: 25
 })
-.type('Eps <b>10</b>');
+.type('Eps <b>11</b>');
 
 new TypeIt('#asuka', {
   speed: 25
@@ -88,9 +88,9 @@ new TypeIt('#reso', {
 new TypeIt('#update', {
   speed: 45
 })
-.type('- Tate no Yuusha no Nariagari Eps 11')
+.type('- Kakegurui×× Eps 11')
 .break()
-.type('- Mob Psycho 100 II Eps 11');
+.type('- Tate no Yuusha no Nariagari Eps 11');
 
 new TypeIt('#info', {
   speed: 45

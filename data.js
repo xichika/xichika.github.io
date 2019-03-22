54dbf414e4b6654b97d99e022cc4fba710015f2c
xichika <47033378+xichika@users.noreply.github.com>
2019-03-22 07:02:03 +0700

    Gotoubun no Hanayome 11

--- data.js
+++ data.js
@@ -31,7 +31,7 @@ new TypeIt('#gaf', {
 new TypeIt('#gnh', {
   speed: 25
 })
-.type('Eps <b>10</b>');
+.type('Eps <b>11</b>');
 
 new TypeIt('#grimms', {
   speed: 25
@@ -88,6 +88,8 @@ new TypeIt('#reso', {
 new TypeIt('#update', {
   speed: 45
 })
+.type('- Gotoubun no Hanayome Eps 11')
+.break()
 .type('- Yakusoku no Neverland Eps 11')
 .break()
 .type('- Girly Air Force Eps 11')

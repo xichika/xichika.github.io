6aa1fe1534fbddc057b741c43b84df7255e100e7
xichika <47033378+xichika@users.noreply.github.com>
2019-03-29 04:34:01 +0700

    Update JS

--- data.js
+++ data.js
@@ -1,3 +1,38 @@
+new TypeIt('#bungo', {
+  speed: 25
+})
+.type('<b>Upcoming</b>');
+
+new TypeIt('#fgone', {
+  speed: 25
+})
+.type('<b>Upcoming</b>');
+
+new TypeIt('#kenja', {
+  speed: 25
+})
+.type('<b>Upcoming</b>');
+
+new TypeIt('#fruits', {
+  speed: 25
+})
+.type('<b>Upcoming</b>');
+
+new TypeIt('#yaiba', {
+  speed: 25
+})
+.type('<b>Upcoming</b>');
+
+new TypeIt('#botak', {
+  speed: 25
+})
+.type('<b>Upcoming</b>');
+
+new TypeIt('#kyojin', {
+  speed: 25
+})
+.type('<b>Upcoming</b>');
+
 new TypeIt('#boogiepop', {
   speed: 25
 })
@@ -18,27 +53,12 @@ new TypeIt('#dororo', {
 })
 .type('Eps <b>12</b>');
 
-new TypeIt('#egao', {
-  speed: 25
-})
-.type('Eps <b>12</b>');
-
-new TypeIt('#gaf', {
-  speed: 25
-})
-.type('Eps <b>11</b>');
-
 new TypeIt('#gnh', {
   speed: 25
 })
 .type('Eps <b>11</b>');
 
 new TypeIt('#grimms', {
-  speed: 25
-})
-.type('Eps <b>11</b>');
-
-new TypeIt('#kkg', {
   speed: 25
 })
 .type('Eps <b>11</b>');
@@ -68,34 +88,40 @@ new TypeIt('#wz', {
 })
 .type('Eps <b>11</b>');
 
-new TypeIt('#ynn', {
-  speed: 25
-})
-.type('Eps <b>11</b>');
-
-new TypeIt('#reso', {
-  speed: 25
-})
-.type('- [HADE 10bit] adalah 720p')
-.break()
-.type('- [S-HADE] adalah 1080p');
-
 new TypeIt('#update', {
+  speed: 45
+})
+.type('Halaman ini masih dalam tahap pengeditan')
+.break()
+.type('List Anime akan ditambahkan jika ada lagi anime yg bagus')
+.break()
+.type('Halaman ini akan mulai berjalan update jika anime musim semi (spring) tiba');
+
+new TypeIt('#oldupdate', {
   speed: 45
 })
 .type('- Girly Air Force Eps 12 [END]')
 .break()
 .type('- Yakusoku no Neverland Eps 12 [END]');
 
+new TypeIt('#reso', {
+  speed: 25
+})
+.type('- <b>[HADE 10bit]</b> adalah resolusi 720p')
+.break()
+.type('- <b>[S-HADE]</b> adalah resolusi 1080p');
+
 new TypeIt('#info', {
   speed: 45
 })
-.type('Dalam koleksi ini gw cuma upload video anime beresolusi 720p dan 1080p saja.')
+.type('Situs koleksi ini cuma upload video anime beresolusi 720p dan 1080p saja.')
 .break()
 .type('Jika ente bahlul dan suka download resolusi burem seperti 360p/480p maka bukan disini tempatnya gaes dan disini tidak update anime episode banyak seperti One Piece, Boruto, Black Clover atau Fairy Tail karena udah ketinggalan jauh hhhh')
 .break()
 .break()
-.type('Semua link download disini memakai google drive biar joss. Untuk file list episodenya akan otomatis update sendiri jika videonya diupload.')
+.type('Semua link download di situs ini memakai google drive biar lebih joss.')
+.break()
+.type('Untuk file list episodenya akan otomatis update sendiri jika videonya diupload.')
 .break()
 .break()
 .type('Jika ente bingung dengan episode yang baru rilis itu episode berapa, silahkan masuk ke situs dibawah ini untuk melihat info episode terbarunya:')

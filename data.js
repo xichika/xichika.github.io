e475c8eb557204f76b21e07a96b901163a85ded2
xichika <47033378+xichika@users.noreply.github.com>
2019-04-14 02:03:24 +0700

    Update data.js

--- data.js
+++ data.js
@@ -21,7 +21,7 @@ new TypeIt('#fruits', {
 new TypeIt('#yaiba', {
   speed: 25
 })
-.type('Eps <b>01</b>');
+.type('Eps <b>02</b>');
 
 new TypeIt('#midara', {
   speed: 25
@@ -66,11 +66,9 @@ new TypeIt('#wz', {
 new TypeIt('#update', {
   speed: 45
 })
-.type('- Fruits Basket Eps 02')
+.type('- Kimetsu no Yaiba Eps 02')
 .break()
-.type('- Midara na Ao-chan wa Benkyou ga Dekinai Eps 02')
-.break()
-.type('- Bungo Stray Dogs S3 Eps 01');
+.type('- Fruits Basket Eps 02');
 
 new TypeIt('#oldupdate', {
   speed: 45

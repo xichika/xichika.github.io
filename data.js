2eccbd5f3e630a13ba307e9992a67c45503a997b
xichika <47033378+xichika@users.noreply.github.com>
2019-04-26 23:33:09 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 new TypeIt('#bungo', {
   speed: 25
 })
-.type('Eps <b>02</b>');
+.type('Eps <b>03</b>');
 
 new TypeIt('#fgone', {
   speed: 25
@@ -66,9 +66,7 @@ new TypeIt('#wz', {
 new TypeIt('#update', {
   speed: 45
 })
-.type('- Kenja no Mago Eps 03')
-.break()
-.type('- Tate no Yuusha no Nariagari Eps 16');
+.type('- Bungo Stray Dogs S3 Eps 03');
 
 new TypeIt('#oldupdate', {
   speed: 45

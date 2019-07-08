f8a02dc17f0e0e4a2505fbf70778570e32a729ef
xichika <47033378+xichika@users.noreply.github.com>
2019-07-08 22:41:15 +0000

    Update demo.js

--- 2019/summer/Vinland-Saga/demo.js
+++ 2019/summer/Vinland-Saga/demo.js
@@ -1,3 +1,3 @@
 <!–
-document.writeln(“| <a href=\”/beranda.asp\”>Beranda</a> | <a href=\”/tentang.asp\”>Tentang</a> | <a href=\”/kontak.asp\”>Informasi Kontak</a> |”);
+document.write(“| <a href=\”/beranda.asp\”>Beranda</a> | <a href=\”/tentang.asp\”>Tentang</a> | <a href=\”/kontak.asp\”>Informasi Kontak</a> |”);
 //–>

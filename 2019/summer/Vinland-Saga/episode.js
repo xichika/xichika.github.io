dcd42487b11bde4081d94cf0e70ff9e99ba29705
xichika <47033378+xichika@users.noreply.github.com>
2019-07-08 22:23:10 +0000

    Update episode.js

--- 2019/summer/Vinland-Saga/episode.js
+++ 2019/summer/Vinland-Saga/episode.js
@@ -1,1 +1,2 @@
-document.write( '<div class=\"schedule-page-item\"><div class=\"schedule-page-show\"><a title=\"List Anime Date A Live III\" href=\"/DAL3/\"><i class=\"fa fa-file fa-fw\"></i> <strong>Date A Live III</strong></a></div></div>' );
+var link = '<div class=\"schedule-page-item\"><div class=\"schedule-page-show\"><a title=\"List Anime Date A Live III\" href=\"/DAL3/\"><i class=\"fa fa-file fa-fw\"></i> <strong>Date A Live III</strong></a></div></div>'
+document.write('link');

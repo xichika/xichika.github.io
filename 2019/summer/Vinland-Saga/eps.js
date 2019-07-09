06813a77b7ca9dc3447c70e5c51c48bbacb89ac2
xichika <47033378+xichika@users.noreply.github.com>
2019-07-09 17:09:28 +0000

    Update eps.js

--- 2019/summer/Vinland-Saga/eps.js
+++ 2019/summer/Vinland-Saga/eps.js
@@ -1,9 +1,9 @@
 var div = document.createElement('div');
-div.innerHTML = "<div class=\"schedule-page-show\"><a href=\"https://drive.google.com/a/student.sbccd.edu/file/d/18bcwZ09THM2EYX-5c3jQUDe_bB57hjOY/view\" title=\"Download\"><i class=\"fa fa-file fa-fw\"></i> <strong>VSG 01 [HADE 10bit] [D7E686D9]</strong></a></div>";
-div.setAttribute('class', 'schedule-page-item');
+div.innerHTML = "<a href=\"https://drive.google.com/a/student.sbccd.edu/file/d/18bcwZ09THM2EYX-5c3jQUDe_bB57hjOY/view\" title=\"Download\"><i class=\"fa fa-file fa-fw\"></i> <strong>VSG 01 [HADE 10bit] [D7E686D9]</strong></a>";
+div.setAttribute('class', 'schedule-page-show');
 document.body.appendChild(div);
 
 var div = document.createElement('div');
 div.innerHTML = "<a href=\"https://google.com\">Google Click 2</a>";
-div.setAttribute('class', 'demo');
+div.setAttribute('class', 'schedule-page-show');
 document.body.appendChild(div);
